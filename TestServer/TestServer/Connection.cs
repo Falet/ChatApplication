2c72bda10ee6663a08b82c48cc553f9ecdac91ea
Falet <miha.m1@yandex.ru>
2020-11-19 13:40:07 +0700

    Test

---
+++ TestServer/TestServer/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestServer
+{
+    class Connection
+    {
+        WebSocketServer server = new WebSocketServer(IPAddress.Any, 65000, false);
+        server.AddWebSocketService<WsConnection>("/",
+                client =>
+                {
+                    client.AddServer(this);
+                });
+    }
+}
--- TestServer/TestServer/Program.cs
+++ TestServer/TestServer/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
-using System.ComponentModel.DataAnnotations;
+using System.Net;
+using WebSocketSharp.Server;
 
 namespace TestServer
 {
@@ -12,59 +8,8 @@ namespace TestServer
     {
         static void Main(string[] args)
         {
-            using (var db = new DBChat())
-            {
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-                var blog = new Rooms { Type = name };
-                db.PoolRoom.Add(blog);
-                db.SaveChanges();
-
-
-
-                // Display all Blogs from the database
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in db.PoolRoom)
-                {
-                    Console.WriteLine(item.Type);
-                }
-
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-            }
+           
+            Console.WriteLine("Hello World!");
         }
-    }
-    public class Rooms
-    {
-        [Key]
-        public int RoomID { get; set; }
-
-        public string Type { get; set; }
-
-        public virtual List<Characters> ListCharacter { get; set; }
-        public virtual List<Messages> PoolMessage { get; set; }
-    }
-
-    public class Characters
-    {
-        [Key]
-        public string CharacterID { get; set; }
-        public int RoomID { get; set; }
-    }
-    public class Messages
-    {
-        [Key]
-        public string MessageID { get; set; }
-        public int RoomID { get; set; }
-        public string From { get; set; }
-        public string Text { get; set; }
-        public string Time { get; set; }
-    }
-    public class DBChat : DbContext
-    {
-        public DbSet<Rooms> PoolRoom { get; set; }
-        /*public DbSet<Characters> ListCharacters { get; set; }
-        public DbSet<Messages> PoolMessages { get; set; }*/
     }
 }
