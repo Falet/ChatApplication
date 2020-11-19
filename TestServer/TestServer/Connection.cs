1f5f55e10e6091719848da4edb05ab10c5549af3
Falet <miha.m1@yandex.ru>
2020-11-19 13:40:33 +0700

    test

--- TestServer/TestServer/Connection.cs
+++ TestServer/TestServer/Connection.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using WebSocketSharp.Server;
 
 namespace TestServer
 {
     class Connection
     {
         WebSocketServer server = new WebSocketServer(IPAddress.Any, 65000, false);
-        server.AddWebSocketService<WsConnection>("/",
-                client =>
-                {
-                    client.AddServer(this);
-                });
+        server.AddWebSocketService<ChatG>("/",client =>{client.AddServer(this);});
     }
 }
